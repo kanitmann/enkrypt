41948560d2b54c49f0498d4198f4fcce322837dd
kanitmann <kanitmann90@gmail.com>
2022-04-06 15:19:31 +0530

    Updated Component call

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import { Navbar, Welcome, Footer, Services, Transactions } from './Components';
+// import  Navbar, Welcome, Footer, Services, Transactions  from './Components';
+import Navbar from './Components/Navbar';
+import Welcome from './Components/Welcome';
+import Footer from './Components/Footer';
+import Services from './Components/Services';
+import Transactions from './Components/Transactions';
 
 const App = () => {
 
   return (
     <div className="min-h-screen">
       <div className="gradient-bg-welcome">
+        <h1>Somethings's wrong I can feel it</h1>
         <Navbar />
         <Welcome />
       </div>
