3a2747040e33c85a74a7617f27522a65e3609a00
kanitmann <kanitmann90@gmail.com>
2022-04-06 14:45:04 +0530

    Components added :books:

---
+++ client/src/Components/index.js
@@ -0,0 +1,6 @@
+import { default as Loader } from './Loader';
+import { default as Footer } from './Footer';
+import { default as Navbar } from './Navbar';
+import { default as Services } from './Services';
+import { default as Transactions } from './Transactions';
+import { default as Welcome } from './Loader';
